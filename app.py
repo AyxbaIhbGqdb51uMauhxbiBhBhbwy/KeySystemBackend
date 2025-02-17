d91e74f2536c396e006ce7f57fa9dffd596018b6
AyxbaIhbGqdb51uMauhxbiBhBhbwy <cateringbilalonline@gmail.com>
2025-02-17 19:18:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -21,24 +21,29 @@ def parse_expired_time(expired_str):
         return now + timedelta(days=int(expired_str[:-1]) * 30)
     elif expired_str.endswith("y"):
         return now + timedelta(days=int(expired_str[:-1]) * 365)
-    return now + timedelta(days=1)
+    return now + timedelta(days=1)  # Default 1 hari jika tidak ada parameter expired yang valid
 
-def calculate_expired_in(expired_time):
+def calculate_expired_in_hours(expired_time):
     time_left = expired_time - datetime.now()
-    return round(time_left.total_seconds(), 2)
+    return round(time_left.total_seconds() / 3600, 2)  # Ubah ke jam
 
 @app.route('/generate', methods=['GET'])
 def generate_key():
     expired_str = request.args.get('expired', '1d')
     expired_time = parse_expired_time(expired_str)
 
+    service = request.args.get('service', 'StarX')  # Default jika service tidak diisi adalah 'StarX'
+    service = ''.join(e for e in service if e.isalnum())  # Menghapus karakter khusus untuk keamanan
+
     random_part = generate_random_string()
-    key = f"StarX_{random_part}"
+    key = f"{service}_{random_part}"  # Format key dengan service yang diinputkan
+
     generated_keys[key] = expired_time
 
     return jsonify({
         "key": key,
-        "expired_in": f"{calculate_expired_in(expired_time)} seconds"
+        "service": service,
+        "expired_in": f"{calculate_expired_in_hours(expired_time)} hours"
     })
 
 @app.route('/check', methods=['GET'])
@@ -49,9 +54,9 @@ def check_key():
 
     if key in generated_keys:
         expired_time = generated_keys[key]
-        expired_in = calculate_expired_in(expired_time)
+        expired_in = calculate_expired_in_hours(expired_time)
         if expired_in > 0:
-            return jsonify({"status": "key is valid", "expired_in": f"{expired_in} seconds"})
+            return jsonify({"status": "key is valid", "expired_in": f"{expired_in} hours"})
         else:
             return jsonify({"status": "key is invalid", "message": "Key has expired."}), 404
     else:
@@ -59,7 +64,7 @@ def check_key():
 
 @app.route('/data', methods=['GET'])
 def get_data():
-    keys_data = {key: f"{calculate_expired_in(expired_time)} seconds"
+    keys_data = {key: f"{calculate_expired_in_hours(expired_time)} hours"
                  for key, expired_time in generated_keys.items()}
     return jsonify({"keys": keys_data})
 
